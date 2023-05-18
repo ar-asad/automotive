4891955ce95947d54e5eb179fbfa79a56fef7e77
arasad1 <asaduzzamanaj07@gmail.com>
2023-05-18 13:17:23 +0600

    footer section added

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -6,7 +6,7 @@ const Banner = () => {
         <div className="hero min-h-screen hero-img" style={{ backgroundImage: `url(${herocar})` }}>
             <div className="hero-overlay bg-opacity-20"></div>
             <div className="text-start flex justify-start text-neutral-content">
-                <div className="max-w-md z-10">
+                <div className="max-w-md px-3">
                     <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
                     <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                     <button className="btn btn-success text-gray-100 bg-green-500 rounded">Get Started &rarr;</button>
